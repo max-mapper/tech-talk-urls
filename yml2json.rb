ef5d772c970e5231883087769d85482c64e252d9
maxogden <max@maxogden.com>
2011-01-03 23:58:18 -0800

    pretty print the datasets

--- yml2json.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'yaml'
-require 'json'
-require 'ap'
-
-Dir.glob('*.yml').each {|yml| File.open("#{yml.gsub('.yml','')}.json", 'w') {|f| f.write({"docs" => YAML.load_file(yml)}.to_json) }}
